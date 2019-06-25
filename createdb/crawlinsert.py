a4ac818a5c96454d4e8258f9eaaf44dad9f56f8c
eldams <damien.nouvel@inalco.fr>
2019-06-25 15:19:42 +0200

    Improvements

--- createdb/crawlinsert.py
+++ createdb/crawlinsert.py
@@ -6,78 +6,90 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 from wiktionaryparser import WiktionaryParser
 
-
 # Récupération de la page avec BS
-my_url = ['https://fr.wiktionary.org/wiki/Cat%C3%A9gorie:Termes_vulgaires_en_anglais','https://fr.wiktionary.org/w/index.php?title=Cat%C3%A9gorie:Termes_vulgaires_en_anglais&pagefrom=rim#mw-pages']
+my_urls = {
+    'en' : ['https://fr.wiktionary.org/wiki/Cat%C3%A9gorie:Termes_vulgaires_en_anglais', 'https://fr.wiktionary.org/w/index.php?title=Cat%C3%A9gorie:Termes_vulgaires_en_anglais&pagefrom=rim#mw-pages'],
+    'fr': ['https://fr.wiktionary.org/wiki/Cat%C3%A9gorie:Termes_vulgaires_en_fran%C3%A7ais','https://fr.wiktionary.org/w/index.php?title=Cat%C3%A9gorie:Termes_vulgaires_en_fran%C3%A7ais&pagefrom=connasse#mw-pages','https://fr.wiktionary.org/w/index.php?title=Cat%C3%A9gorie:Termes_vulgaires_en_fran%C3%A7ais&pagefrom=foutre#mw-pages','https://fr.wiktionary.org/w/index.php?title=Cat%C3%A9gorie:Termes_vulgaires_en_fran%C3%A7ais&pagefrom=ratisser+le+bunker#mw-pages'],
+    'es': ['https://fr.wiktionary.org/wiki/Cat%C3%A9gorie:Termes_vulgaires_en_espagnol'],
+    'it': ['https://fr.wiktionary.org/wiki/Cat%C3%A9gorie:Termes_vulgaires_en_italien'],
+}
 
-# Préparation du parser
-parser = WiktionaryParser()
-parser.set_default_language('english')
-parser.include_relation('alternative forms')
+for lang in my_urls:
 
-epi = epitran.Epitran('eng-Latn')
+    if lang == 'en':
+        # Préparation du parser
+        parser = WiktionaryParser()
+        parser.set_default_language('english')
+        parser.include_relation('alternative forms')
+        epi = epitran.Epitran('eng-Latn')
 
-# Préparation de la base de données
-import base
+    if lang == 'fr':
+        # Préparation du parser
+        parser = WiktionaryParser()
+        parser.set_default_language('french')
+        parser.include_relation('alternative forms')
+        epi = epitran.Epitran('fra-Latn')
 
-for url in my_url:
+    # Préparation de la base de données
+    import base
 
-    uClient = uReq(url)
-    page_html = uClient.read()
+    for url in my_url:
 
-    page_soup = soup(page_html, "html.parser")
+        uClient = uReq(url)
+        page_html = uClient.read()
 
-    parser = WiktionaryParser()
+        page_soup = soup(page_html, "html.parser")
 
-    for div in page_soup.findAll("div", {"class":"mw-category-group"}):
-        for ul in div.findAll("a"): # parcourir tous les paragraphes
-            # Se débarrasser des lignes vides (sauf la première, je ne sais pas pourquoi)
-            text = ul.text
-            if text == "": # permet de se débarrasser de la première ligne vide
-                continue
-            print('Extraction du mot: ', text)
+        parser = WiktionaryParser()
 
-            wikiparsedlst = parser.fetch(text)
-            for wikiparsed in wikiparsedlst:
-                print(wikiparsed)
-                # for prop in wikiparsed:
-                #     print(prop, wikiparsed[prop])
-                #parser.exclude_part_of_speech('noun')
-                # print(exprgrossiere)
-                etymology = wikiparsed['etymology']
-                definitions = []
-                categorie = []
+        for div in page_soup.findAll("div", {"class":"mw-category-group"}):
+            for ul in div.findAll("a"): # parcourir tous les paragraphes
+                # Se débarrasser des lignes vides (sauf la première, je ne sais pas pourquoi)
+                text = ul.text
+                if text == "": # permet de se débarrasser de la première ligne vide
+                    continue
+                print('Extraction du mot: ', text)
 
-                for definitionlst in wikiparsed['definitions']:
-                    for definition in definitionlst['text']:
-                        definitions.append(definition.strip())
-                    if isinstance(definitionlst, list):
-                        for partofspeech in definitionlst['partOfSpeech']:
-                            print(partofspeech + "\n")
-                    else:
-                        print(definitionlst['partOfSpeech'] + "\n")
-                        categorie.append(definitionlst['partOfSpeech'])
-                        print(categorie)
+                wikiparsedlst = parser.fetch(text)
+                for wikiparsed in wikiparsedlst:
+                    print(wikiparsed)
+                    # for prop in wikiparsed:
+                    #     print(prop, wikiparsed[prop])
+                    #parser.exclude_part_of_speech('noun')
+                    # print(exprgrossiere)
+                    etymology = wikiparsed['etymology']
+                    definitions = []
+                    categorie = []
 
-                pronunciations = []
-                for pronunciation in wikiparsed['pronunciations']['text']:
-                    import re
-                    ipa_match = re.findall(r'IPA *: */([^/]*)/', pronunciation)
-                    new_list = []
-                    for ipa in ipa_match:
-                        new_list.append(ipa)
-                    print(new_list)
-                    script = ''
-                    try:
-                        script = epi.transliterate(text)
-                    except KeyError:
-                        continue
-                pronunciations.append(script.strip())
+                    for definitionlst in wikiparsed['definitions']:
+                        for definition in definitionlst['text']:
+                            definitions.append(definition.strip())
+                        if isinstance(definitionlst, list):
+                            for partofspeech in definitionlst['partOfSpeech']:
+                                print(partofspeech + "\n")
+                        else:
+                            print(definitionlst['partOfSpeech'] + "\n")
+                            categorie.append(definitionlst['partOfSpeech'])
+                            print(categorie)
 
-                base.insert_grossier(text, '|'.join(categorie), '|'.join(pronunciations), '|'.join(definitions), etymology, 'en')
+                    pronunciations = []
+                    for pronunciation in wikiparsed['pronunciations']['text']:
+                        import re
+                        ipa_match = re.findall(r'IPA *: */([^/]*)/', pronunciation)
+                        new_list = []
+                        for ipa in ipa_match:
+                            new_list.append(ipa)
+                        print(new_list)
+                        script = ''
+                        try:
+                            script = epi.transliterate(text)
+                        except KeyError:
+                            continue
+                    pronunciations.append(script.strip())
 
+                    base.insert_grossier(text, '|'.join(categorie), '|'.join(pronunciations), '|'.join(definitions), etymology, lang)
 
-uClient.close()
+    uClient.close()
 
 #['https://fr.wiktionary.org/wiki/Cat%C3%A9gorie:Termes_vulgaires_en_anglais','https://fr.wiktionary.org/w/index.php?title=Cat%C3%A9gorie:Termes_vulgaires_en_anglais&pagefrom=rim#mw-pages','https://fr.wiktionary.org/wiki/Cat%C3%A9gorie:Termes_vulgaires_en_espagnol','https://fr.wiktionary.org/wiki/Cat%C3%A9gorie:Termes_vulgaires_en_fran%C3%A7ais','https://fr.wiktionary.org/w/index.php?title=Cat%C3%A9gorie:Termes_vulgaires_en_fran%C3%A7ais&pagefrom=connasse#mw-pages','https://fr.wiktionary.org/w/index.php?title=Cat%C3%A9gorie:Termes_vulgaires_en_fran%C3%A7ais&pagefrom=foutre#mw-pages','https://fr.wiktionary.org/w/index.php?title=Cat%C3%A9gorie:Termes_vulgaires_en_fran%C3%A7ais&pagefrom=ratisser+le+bunker#mw-pages']
 #['https://fr.wiktionary.org/wiki/Cat%C3%A9gorie:Termes_vulgaires_en_italien']
--- flask/multicoarseweb/flaskapp/views.py
+++ flask/multicoarseweb/flaskapp/views.py
@@ -1,45 +1,21 @@
 from flask import Flask, render_template, request
 import sqlite3
 
-
 app = Flask(__name__)
-
-# Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
-# To get one variable, tape app.config['MY_VARIABLE']
 
 @app.route('/')
-@app.route('/home')
 def home():
    return render_template('home.html') 
 
-@app.route('/result')
-def result():
-    
+@app.route('/searchword', methods=['GET'])
+def result():    
     db=sqlite3.connect('coarsewords.db')
-    print(db)
-    res=db.execute(" select * from coarseword")
-    mots= res.fetchall()
-    lstmots = ''
-    lstmots += '<table>'
-    lstmots += '<tr>'
-    lstmots += '<th style="background-color:rgb(238, 130, 238);">Mot </th>'
-    lstmots += '<th style="background-color:DodgerBlue;">Catégorie </th>'
-    lstmots += '<th style="background-color:DodgerBlue;">Phonetique </td>'
-    lstmots += '<th style="background-color:Tomato;">Definition </td>'
-    lstmots += '<th style="background-color:hsla(9, 100%, 64%, 0.5);">Etymologie </td>'
-    lstmots += '<th style="background-color:rgb(255, 165, 0);">Langue </td>'
-    lstmots += '</tr>'
-    for mot in mots:
-        lstmots += '<tr>'
-        lstmots += '<td style="background-color:rgb(238, 130, 238);">' + mot[0]+'</td>'
-        lstmots += '<td style="background-color:DodgerBlue;">'+ mot[1]+ '</td>'
-        lstmots += '<td style="background-color:DodgerBlue;">'+ mot[2]+ '</td>'
-        lstmots += '<td style="background-color:Tomato;">'+  mot[3]+ '</td>'
-        lstmots += '<td style="background-color:hsla(9, 100%, 64%, 0.5);">' +  mot[4]+ '</td>'
-        lstmots += '<td style="background-color:rgb(255, 165, 0);">'+  mot[5]+ '</td>'
-        lstmots += '</tr>'
-    lstmots += '</table>'
-    return lstmots 
-##if __name__ == "__main__":
-##    app.run()
+    # print(request)
+    # print(request.method)
+    # for key in request.form.keys():
+    #     print(key)
+    # wordsearched = request.form['word']
+    res = db.execute(" select * from coarseword") # WHERE phonetics='wordsearched'
+    words = res.fetchall()
+    return render_template('result.html', words=words) 
