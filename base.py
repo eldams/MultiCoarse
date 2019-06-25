85d30197da7b69879e9a356914263096b4db4d3e
eldams <damien.nouvel@inalco.fr>
2019-06-25 12:28:20 +0200

    Initialization of script to extract from Wiktionnary and insert into DB

---
+++ base.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+conn = None
+cursor = None
+
+def connect():
+    global conn, cursor
+    print('Connect to DB')
+    conn = sqlite3.connect('grossiers2.db')
+    cursor = conn.cursor()
+
+def create_table():
+    print('Recreate table')
+    cursor.execute('DROP TABLE IF EXISTS motsgrossiers;')
+    cursor.execute('CREATE TABLE IF NOT EXISTS motsgrossiers (mot TEXT, catégories TEXT, phonétique TEXT, définition TEXT, étymologie TEXT, langues TEXT )')
+
+def sql_escape(s):
+    return '"'+s.replace('"', '""')+'"'
+
+def insert_grossier(mot = '', categorie = '', phonetique = '', definition = '', etymologie = '', langue = ''):
+    print('Insert grossier')
+    mot = sql_escape(mot)
+    categorie = sql_escape(categorie)
+    phonetique = sql_escape(phonetique)
+    definition = sql_escape(definition)
+    etymologie = sql_escape(etymologie)
+    langue = sql_escape(langue)
+    cursor.execute('INSERT INTO motsgrossiers(mot, catégories, phonétique, définition, étymologie, langues) VALUES ('+mot+','+categorie+','+phonetique+','+definition+','+etymologie+','+langue+')')
+    conn.commit()
+
+connect()
+create_table()
+insert_grossier()
